13ee6505545cd2693755c65f0b801e4ae3fdb197
MaximSelhanovich <75281453+MaximSelhanovich@users.noreply.github.com>
2023-02-24 18:31:02 +0300

    Cycle references are detected

--- Lab3/Development.sql
+++ Lab3/Development.sql
@@ -1,7 +1,22 @@
+DROP TABLE MyChildTable; /
 DROP TABLE MyTable; /
+ALTER TABLE Students DROP CONSTRAINT fk_group_id; /
+ALTER TABLE GROUPS DROP CONSTRAINT fk_new_table_id; /
+ALTER TABLE NEWTABLE DROP CONSTRAINT fk_STUDENT_id; /
 DROP TABLE Students; /
 DROP TABLE Groups; /
 DROP TABLE NEWTABLE; /
+
+CREATE TABLE MyChildTable
+(
+    id NUMBER GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1) NOT NULL PRIMARY KEY,
+    MyTable_id number not null,
+    CONSTRAINT fk_mytable_id
+        FOREIGN KEY (MyTable_id)
+        REFERENCES Groups(id)
+); /
+
+
 CREATE TABLE MyTable 
 (
     id NUMBER GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1) NOT NULL PRIMARY KEY,
@@ -13,14 +28,16 @@ CREATE TABLE MyTable
 
 CREATE TABLE NEWTABLE
 (
-    id number
+    id NUMBER GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1) not null PRIMARY KEY,
+    STUDENT_ID NUMBER NOT NULL
 ); /
 
 CREATE TABLE Groups 
 (
     id NUMBER GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1) NOT NULL PRIMARY KEY,
     name VARCHAR2(200) NOT NULL,
-    c_val NUMBER DEFAULT 0
+    c_val NUMBER DEFAULT 0,
+    new_table_id number not null  
 ); /
 
 CREATE TABLE Students
@@ -31,8 +48,14 @@ CREATE TABLE Students
     CONSTRAINT fk_group_id
         FOREIGN KEY (group_id)
         REFERENCES Groups(id)
-);
-SELECT
-  COUNT(*)
-FROM
-  all_tables;
+); /
+
+ALTER TABLE groups add CONSTRAINT fk_new_table_id
+        FOREIGN KEY (new_table_id)
+        REFERENCES NEWTABLE(id); /
+        
+ALTER TABLE NEWTABLE add CONSTRAINT fk_STUDENT_id
+        FOREIGN KEY (STUDENT_ID)
+        REFERENCES sTUDENTS(id); 
+
+SELECT * FROM all_constraints WHERE OWNER ='C##DEVELOPMENT';
--- Lab3/Lab3.sql
+++ Lab3/Lab3.sql
@@ -96,3 +96,127 @@ SELECT *  FROM all_cons_columns WHERE OWNER = 'C##DEVELOPMENT';
 BEGIN
     check_tables('C##DEVELOPMENT', 'C##PRODUCTION');
 END;
+
+select * from all_constraints;
+select * from all_constraints
+where OWNER = 'C##DEVELOPMENT' or OWNER = 'C##PRODUCTION';
+
+
+/*with table_hierarchy as (
+  select child_owner, child_table, parent_owner, parent_table
+  from (
+    select owner child_owner, table_name child_table,
+    r_owner parent_owner, r_constraint_name constraint_name
+    from ALL_constraints 
+    where constraint_type = 'R'
+  ) join (
+    select owner parent_owner, constraint_name, table_name parent_table
+    from ALL_constraints
+    where constraint_type = 'P'
+  )
+  using(parent_owner, constraint_name)
+)
+select distinct child_owner, child_table from (
+  select * from table_hierarchy
+  where (child_owner, child_table) in (
+    select parent_owner, parent_table from table_hierarchy
+  )
+) a
+where connect_by_iscycle = 1
+connect by nocycle (prior child_owner, prior child_table)
+                = ((parent_owner, parent_table));*/
+
+
+--r_constraint_name имя на которое ссылаемся, ищем его просто в именах constraint_name? и если находим,
+-- то по этому имени ищем таблицу, которой и принадлежит этот constraint
+select *
+from all_constraints
+where r_owner = 'C##DEVELOPMENT'
+and constraint_type = 'R'
+and r_constraint_name in
+ (
+   select constraint_name from all_constraints
+   where constraint_type in ('P', 'U')
+   and owner = 'C##DEVELOPMENT'
+ );
+
+
+CREATE OR REPLACE VIEW parent_child_view AS
+SELECT pk.owner      parent_owner,
+       pk.table_name parent_table,
+       fk.owner      child_owner,
+       fk.table_name child_table
+FROM all_constraints fk,
+     all_constraints pk
+WHERE pk.owner = fk.r_owner
+    AND pk.constraint_name = fk.r_constraint_name
+    AND fk.constraint_type = 'R'
+    AND pk.owner = 'C##DEVELOPMENT';
+
+    SELECT * FROM parent_child_view;
+    
+
+CREATE OR REPLACE PROCEDURE circular_check
+IS
+CURSOR parent_child_cursor IS 
+    SELECT pk.owner parent_owner,
+       pk.table_name parent_table,
+       fk.owner child_owner,
+       fk.table_name child_table
+    FROM all_constraints fk,
+        all_constraints pk
+    WHERE pk.owner = fk.r_owner
+        AND pk.constraint_name = fk.r_constraint_name
+        AND fk.constraint_type = 'R'
+        AND pk.owner = 'C##DEVELOPMENT';
+message VARCHAR2(200);
+BEGIN
+    FOR rec IN parent_child_cursor
+    LOOP
+        message := rec.parent_table || '->' || rec.child_table;
+        DBMS_OUTPUT.PUT_LINE(recursive_part_circular_check(rec.parent_table, 
+                                                            rec.child_table,
+                                                            message));
+        DBMS_OUTPUT.PUT_LINE(message);
+    END LOOP;
+END circular_check;
+
+
+CREATE OR REPLACE FUNCTION recursive_part_circular_check(start_table VARCHAR2, 
+                                          step_table VARCHAR2,
+                                          message IN OUT VARCHAR2) RETURN NUMBER
+IS
+    check_val VARCHAR2(100);
+BEGIN
+    SELECT child_table INTO check_val FROM parent_child_view WHERE parent_table = step_table AND ROWNUM = 1;
+    IF start_table = check_val THEN
+        message := message || '->' || start_table;
+        RETURN 1;
+    ELSE
+        message := message || '->' || check_val;
+        RETURN recursive_part_circular_check(start_table, check_val, message);
+    END IF;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        message := 'no cycle';
+        RETURN 0;
+END recursive_part_circular_check;
+ 
+
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('START');
+    circular_check;
+END;
+
+
+ 
+SELECT pk.owner      parent_owner,
+       pk.table_name parent_table,
+       fk.owner      child_owner,
+       fk.table_name child_table
+FROM all_constraints fk,
+     all_constraints pk
+WHERE pk.owner = fk.r_owner
+    AND pk.constraint_name = fk.r_constraint_name
+    AND fk.constraint_type = 'R'
+    AND pk.owner = 'C##DEVELOPMENT';
